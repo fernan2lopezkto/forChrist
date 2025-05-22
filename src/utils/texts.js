3464ad3198722d42732c4b27a7de4545db40b1f2
Juan Rey <105689962+fernan2lopezkto@users.noreply.github.com>
2025-05-22 16:49:01 -0300

    Update texts.js

--- src/utils/texts.js
+++ src/utils/texts.js
@@ -15,9 +15,50 @@ export const navigationItems = [
   { label: "Apóyanos", href: "/support" },
 ];
 
+// HOME
+export const homeTexts = {
+
+ }
+
+// ABOUT
+export const aboutTexts = {
+
+}
+
+// MATERIAL
+export const materialTexts = {
+
+}
+
+// EVANGELISMO
+export const evangelizmoTexts = {
+
+}
+
+// TUTORIALES
+export const tutorialesTexts = {
+
+}
+
+// CRECIMIENTO
+export const crecimientoTexts = {
+
+}
+
+// APOYANOS
+export const apoyanosTexts = {
+
+}
+
+// TIENDA
+export const tiendaTexts = {
+
+}
+
 // NOMBRE
 export const proyectNames = { long: "4C-ForChrist", short: "4C" };
 
+// Footer
 export const footerText =
   "© 2025 Mi Plataforma Cristiana. Todos los derechos reservados.";
 
